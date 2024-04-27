3ca0de057c48b8039ac3b39deb05f8902f48cade
Tingyu Dong <tingyu.dong@thoughtworks.com>
2024-04-27 19:51:42 +0800

    feat: add test for find book

---
+++ backend/src/test/java/com/example/backend/controller/BooksControllerTest.java
@@ -0,0 +1,40 @@
+package com.example.backend.controller;
+
+import com.example.backend.data.Book;
+import com.example.backend.service.BooksService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(BooksController.class)
+public class BooksControllerTest {
+    @MockBean
+    BooksService booksService;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void testGetById() throws Exception {
+        Book book = new Book().withId(1L).withName("Iliad");
+
+        when(booksService.findById(1L)).thenReturn(book);
+
+        mockMvc.perform(get("/book/1")).andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Iliad"));
+
+        verify(booksService).findById(1);
+
+    }
+}
