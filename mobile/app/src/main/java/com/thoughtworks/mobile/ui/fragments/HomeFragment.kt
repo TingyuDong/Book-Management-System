291f395af589c028a6b1be55c29a95ffc931dc04
Tingyu Dong <tingyu.dong@thoughtworks.com>
2024-04-28 10:44:37 +0800

    [mobile]feat: collectAsStateWithLifecycle in compose

--- mobile/app/src/main/java/com/thoughtworks/mobile/ui/fragments/HomeFragment.kt
+++ mobile/app/src/main/java/com/thoughtworks/mobile/ui/fragments/HomeFragment.kt
@@ -15,11 +15,14 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.fragment.findNavController
 import com.thoughtworks.mobile.R
 import com.thoughtworks.mobile.ui.theme.MobileTheme
@@ -27,7 +30,7 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class HomeFragment : Fragment() {
 
-    private val viewModel: HomeViewModel by viewModel()
+    private val homeViewModel: HomeViewModel by viewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,17 +39,25 @@ class HomeFragment : Fragment() {
         return ComposeView(requireContext()).apply {
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
             setContent {
-                MobileTheme {
-                    Surface(
-                        modifier = Modifier.fillMaxSize(),
-                        color = MaterialTheme.colorScheme.background
-                    ) {
-                        FloatingButton {
-                            findNavController().navigate(R.id.action_home_to_add)
-                        }
-                    }
+                HomeScreen(viewModel=homeViewModel) {
+                    findNavController().navigate(R.id.action_home_to_add)
                 }
             }
+        }
+    }
+}
+
+@OptIn(ExperimentalLifecycleComposeApi::class)
+@Composable
+fun HomeScreen(viewModel: HomeViewModel, onClick: () -> Unit) {
+    val uiState: HomeUiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    MobileTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            FloatingButton(onClick = onClick)
         }
     }
 }
