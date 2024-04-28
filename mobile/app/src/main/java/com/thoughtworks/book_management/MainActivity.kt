0ceb066153cf7c97d054d94352c7968aa33133e5
Tingyu Dong <tingyu.dong@thoughtworks.com>
2024-04-28 15:06:12 +0800

    [mobile]feat: delete redundant code

--- mobile/app/src/main/java/com/thoughtworks/book_management/MainActivity.kt
+++ mobile/app/src/main/java/com/thoughtworks/book_management/MainActivity.kt
@@ -2,6 +2,7 @@ package com.thoughtworks.book_management
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.thoughtworks.mobile.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
